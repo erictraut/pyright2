dba0ae0c9e986e49bb3f50fcb87d08bb7925d1e1
Eric Traut <eric@traut.com>
2025-06-10 21:10:11 -0700

    Started to reorganize code to do the big split between type checker and language server.

---
+++ packages/typechecker/src/analyzer/commands.ts
@@ -0,0 +1,13 @@
+/*
+ * commands.ts
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ * Author: Eric Traut
+ *
+ * Command identifier strings.
+ */
+
+export const enum Commands {
+    createTypeStub = 'pyright.createtypestub',
+    unusedImport = 'pyright.unusedImport',
+}
